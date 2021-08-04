256d10b454511863461e48df426cd68d10710991
troymeeker <troymeeker1@gmail.com>
2021-08-04 09:47:47 -0700

    complete

--- js/challenge.js
+++ js/challenge.js
@@ -6,34 +6,47 @@ document.addEventListener('DOMContentLoaded',() => {
  const resumeTimer = document.querySelector('#resume')
 const heart = document.querySelector('#heart')
 const likes = document.querySelector("ul.likes")
-let timeLeft = 0
+// let timeLeft = 0
 let numberTracker= {}
+const commenter = document.querySelector('#comment-form')
+const comments = document.querySelector('#list')
 
 let interval = setInterval(increase, 1000)
 
 pauseTimer.addEventListener('click', function(){
-   clearInterval(interval) 
+     if(pauseTimer.id==="resume"){
+      interval = setInterval(increase,1000)
+      pauseTimer.innerText = "pause"
+      pauseTimer.id = "pause"
+   }else if(pauseTimer.id==="pause"){
+      clearInterval(interval) 
    pauseTimer.innerText = 'Resume'
    pauseTimer.id = 'resume'
-})
+}
+   })
+
+
 plusOne.addEventListener('click', increase)
 minusOne.addEventListener('click', decrease)
 heart.addEventListener('click', addLike)
+
+//increase counter
 function increase(e){
    counterNum.innerText = parseInt(counter.innerText) + 1
 }
-
+//decrease counter
 function decrease(){
    counterNum.innerText = parseInt(counter.innerText) - 1
 }
-
+//add like
 function addLike(){
    let second = counterNum.innerText
    numberTracker[second]= numberTracker[second] || 0 
    numberTracker[second] +=1
-   renLikes()
+   showLikes()
 }
-function renLikes(){
+
+function showLikes(){
    likes.innerHTML = ""
    for(let key in numberTracker){
       const li = document.createElement('li')
@@ -42,4 +55,19 @@ function renLikes(){
    }
 }
 
-});
+commenter.addEventListener('submit', addComment)
+
+function addComment(e){
+   e.preventDefault();
+
+   const comment = e.target.querySelector('#comment-input').value
+   const li = document.createElement('li')
+
+   li.textContent = `${comment}` ;
+  const commentSection =  document.querySelector('#comments')
+   commentSection.appendChild(li)
+   console.log(li)
+
+//   document.querySelector('#comments').appendChild();
+}
+})
